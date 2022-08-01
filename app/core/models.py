a64b7382c3b8e2525786e35c69537675de45256a
Mark Patterson: laptop <markpattersonesq@gmail.com>
2022-08-01 14:59:49 +1000

    Added user profile admin

--- app/core/models.py
+++ app/core/models.py
@@ -12,19 +12,29 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users"""
 
-    def create_user(self, email, password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.Charfield(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
     objects = UserManager()
