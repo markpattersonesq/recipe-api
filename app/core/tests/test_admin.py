4fd62922476bc27919d46af0998ff1315f9f6f59
Mark Patterson: laptop <markpattersonesq@gmail.com>
2022-08-01 15:07:29 +1000

    Fixed linting

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -18,9 +18,9 @@ class AdminSiteTests(TestCase):
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'testpass123',
-            name = 'Test User'
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
         )
 
     def test_users_list(self):
